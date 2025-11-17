7291b00475083ddd3611a09637c4012536794cf9
sg.balagna <sg.balagna@mail.scuole.vda.it>
2025-11-17 18:49:32 +0100

    Tentativo 4

--- model/model.py
+++ model/model.py
@@ -1,3 +1,6 @@
+import copy
+
+from database.consumo_DAO import ConsumoDAO
 from database.impianto_DAO import ImpiantoDAO
 
 '''
@@ -26,6 +29,26 @@ class Model:
         :return: lista di tuple --> (nome dell'impianto, media), es. (Impianto A, 123)
         """
         # TODO
+        consumi=[]
+        a=ConsumoDAO.get_consumi(1)
+        b=ConsumoDAO.get_consumi(2)
+        media=0
+        i=0
+        for c in a:
+            if c.data.month==mese:
+                media=c.kwh+media
+                i=i+1
+        media=media/i
+        consumi.append(("Impianto A",media))
+        media = 0
+        i=0
+        for c in b:
+            if c.data.month==mese:
+                media=c.kwh+media
+                i=i+1
+        media=media/i
+        consumi.append(("Impianto B",media))
+        return consumi
 
     def get_sequenza_ottima(self, mese:int):
         """
@@ -47,6 +70,52 @@ class Model:
     def __ricorsione(self, sequenza_parziale, giorno, ultimo_impianto, costo_corrente, consumi_settimana):
         """ Implementa la ricorsione """
         # TODO
+        if giorno > 7:
+            if  self.__costo_ottimo == -1 or self.__costo_ottimo > int(costo_corrente):
+                self.__costo_ottimo = costo_corrente
+                self.__sequenza_ottima = copy.deepcopy(sequenza_parziale)
+            #print(sequenza_parziale)
+            #print(self.__costo_ottimo)
+            #print(self.__sequenza_ottima)
+        else:
+            for j in range(1,3):
+                old_ultimo = ultimo_impianto
+                old_costo = costo_corrente
+                if ultimo_impianto == None or ultimo_impianto== j:
+                    u=0
+                else:
+                    u=5
+                sequenza_parziale.append(j)
+                ultimo_impianto = j
+                costo_corrente += consumi_settimana.get(j)[giorno-1]+u
+                self.__ricorsione(sequenza_parziale,giorno+1,ultimo_impianto,costo_corrente,consumi_settimana)
+                sequenza_parziale.pop()
+                ultimo_impianto = old_ultimo
+                costo_corrente = old_costo
+
+            """
+            a=0
+            b=0
+            if ultimo_impianto==1:
+                b=5
+            elif ultimo_impianto==2:
+                a=5
+            #print(consumi_settimana.get(1)[giorno-1])
+            #print(a,b)
+            if (consumi_settimana.get(1)[giorno-1]+a)<(consumi_settimana.get(2)[giorno-1]+b):
+
+                sequenza_parziale.append(1)
+                ultimo_impianto=1
+                costo_corrente=costo_corrente+consumi_settimana.get(1)[giorno-1]+a
+            else:
+                sequenza_parziale.append(2)
+                ultimo_impianto=2
+                costo_corrente=costo_corrente+consumi_settimana.get(2)[giorno-1]+b
+            #print(costo_corrente)
+            print(sequenza_parziale)
+            self.__ricorsione(sequenza_parziale,giorno+1,ultimo_impianto,costo_corrente,consumi_settimana)
+            """
+
 
     def __get_consumi_prima_settimana_mese(self, mese: int):
         """
@@ -54,4 +123,20 @@ class Model:
         :return: un dizionario: {id_impianto: [kwh_giorno1, ..., kwh_giorno7]}
         """
         # TODO
+        a = ConsumoDAO.get_consumi(1)
+        b = ConsumoDAO.get_consumi(2)
+        somma = []
+        for c in a:
+            if c.data.month == mese:
+                if c.data.day<=7:
+                    somma.append(c.kwh)
+        somma_b = []
+        for c in b:
+            if c.data.month == mese:
+                if c.data.day<=7:
+                    somma_b.append(c.kwh)
+        consumi={1:somma, 2:somma_b}
+        #print(consumi)
+        return consumi
+
 
